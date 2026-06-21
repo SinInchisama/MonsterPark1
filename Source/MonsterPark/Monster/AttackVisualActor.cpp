f6617cce42b490414ea5c21bb89244a50afadcbb
Sinichisama <113009469+SinInchisama@users.noreply.github.com>
2026-06-21 20:17:30 +0900

    성벽 밖 유닛도 피통 보이게 만들었음

--- Source/MonsterPark/Monster/AttackVisualActor.cpp
+++ Source/MonsterPark/Monster/AttackVisualActor.cpp
@@ -1,14 +1,26 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include"MonsterPark/Monster/AttackVisualActor.h"
+#include "MonsterPark/Monster/AttackVisualActor.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Components/SceneComponent.h"
+#include "Components/WidgetComponent.h"
 #include "Animation/AnimInstance.h"
 
 AAttackVisualActor::AAttackVisualActor()
 {
-    MeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComponent"));
-    RootComponent = MeshComponent;
+	DummyRoot = CreateDefaultSubobject<USceneComponent>(TEXT("DummyRoot"));
+	RootComponent = DummyRoot;
+
+	MeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(DummyRoot);
+
+	HealthBarComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("HealthBarComponent"));
+	HealthBarComponent->SetupAttachment(DummyRoot);
+
+	HealthBarComponent->SetWidgetSpace(EWidgetSpace::Screen);
+	HealthBarComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 130.0f)); 
+	HealthBarComponent->SetDrawSize(FVector2D(100.0f, 15.0f));
 
     PrimaryActorTick.bCanEverTick = false;
 }
@@ -42,4 +54,8 @@ void AAttackVisualActor::PlayDeathAnimation()
             AnimInstance->Montage_Play(DeathMontage);
         }
     }
+}
+
+void AAttackVisualActor::UpdateHealthUI_Implementation(float HealthPercent)
+{
 }
